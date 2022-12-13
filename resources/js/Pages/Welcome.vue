602ec6db9807df0cb3a0d0e007fdb525bf2c33ef
noahsaber <moahmed.bikiri@univ-constantine2.dz>
2022-12-13 16:18:44 +0100

    Saber

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,14 +13,8 @@ defineProps({
     <Head title="Welcome" />
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.auth.user" :href="route('partner.dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
-
-            <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
-
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
-            </template>
+        <div>
+            hello there
         </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
